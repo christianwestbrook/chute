8be880a8d44de211cf91b508eaf18daa27abe0f0
Cw <cw@props.org>
2020-08-07 10:38:46 -0700

    Initial commit of README + index.php.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+// On the bitchute side (no slash):
+$bc = "https://www.bitchute.com/video";
+
+// On this site:
+$uri = $_SERVER[ 'REQUEST_URI' ];
+$bcUrl = $bc . $uri;
+
+if( strlen( $uri ) == 1 ) {
+    echo "Use this site to get around Twitter's censorship of Bitchute videos.<br/><br/>For example, twitter won't let me share <pre>https://www.bitchute.com/video/<b>pQ84E954pkgj</b>/</pre>  Instead share <pre>http://chute.props.org/<b>pQ84E954pkgj</b></pre>";
+    exit();
+}
+
+// Who's calling please?
+$agent = $_SERVER['HTTP_USER_AGENT'];
+//file_put_contents('/tmp/chute-agents.txt', $agent . "\n", FILE_APPEND);
+
+// Is it "Twitterbot/1.0."
+if(  stripos( $agent, "Twitterbot" ) !== false ) {
+    // It's the communists at twitter!  Proxy the data, don't let them know it's bitchute underneath.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $bcUrl);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $res = curl_exec($ch);
+    //$code = curl_getinfo($ch,  CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    echo $res;
+} else {
+    // This is a real person clicking a link -- just redirect them to bitchute.
+    Header("Location:  $bcUrl");
+}
+
+
